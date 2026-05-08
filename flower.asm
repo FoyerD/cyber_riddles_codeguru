a9dc5a4446f9ac23ea4fb0d1bac84634b0750bad
FoyerD <amitfoye@post.bgu.ac.il>
2026-05-08 10:55:48 +0300

    added putpixel

--- flower.asm
+++ flower.asm
@@ -34,3 +34,91 @@ finish:
     jmp finish
 
 
+put_pixels:
+    ; ax=xc, bx=yc, cx=x, dx=y
+    push bp
+    mov bp, sp
+
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+
+
+    mov cl, 8
+    
+    mov si, ax
+    add si, cx
+    mov di, bx
+    add di, dx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    sub si, cx
+    mov di, bx
+    add di, dx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    add si, cx
+    mov di, bx
+    sub di, dx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    sub si, cx
+    mov di, bx
+    sub di, dx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    add si, dx
+    mov di, bx
+    add di, cx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    sub si, dx
+    mov di, bx
+    add di, cx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    add si, dx
+    mov di, bx
+    sub di, cx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+    
+    mov si, ax
+    sub si, dx
+    mov di, bx
+    sub di, cx
+    shl di, cl
+    add di, si
+    mov byte [di], 1
+
+    
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
